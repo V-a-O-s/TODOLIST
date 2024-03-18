47734d255c860b755d823b16fc105b128952704d
V-a-O-s <asteorit01@googlemail.com>
2024-03-18 20:53:18 +0100

    Added Test for findById and taskCreation

---
+++ backend/src/test/java/com/example/demo/controller/TestTaskController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTaskController {
+    @Test
+    public void testSetTaskDescription(){
+        assertTrue(true,"RestController");
+    }
+
+}
--- backend/src/test/java/com/example/demo/repository/TaskRepositoryTest.java
+++ backend/src/test/java/com/example/demo/repository/TaskRepositoryTest.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -26,5 +28,23 @@ public class TaskRepositoryTest {
         Task findMe = taskRepository.findByTaskdescription(test_me);
         assertNotNull(findMe,"Task should have been found.");
         assertEquals(test_me, findMe.getTaskdescription());
+    }
+
+    @Test
+    void testFindById() {
+        // Given
+        Task task = new Task();
+        task.setTaskdescription("Task 1");
+        taskRepository.save(task);
+
+        int taskId = task.getId();
+        String expectedDescription = task.getTaskdescription();
+
+        Optional<Task> optionalTask = taskRepository.findById(taskId);
+
+        assertTrue(optionalTask.isPresent());
+        Task foundTask = optionalTask.get();
+        assertEquals(taskId, foundTask.getId());
+        assertEquals(expectedDescription, foundTask.getTaskdescription());
     }
 }
